81d0f1af42de51cca0eba7506c0f9ee53046e70c
Nguyen Duc Hiep <hiep.nd@gmail.com>
2022-05-19 13:07:28 +0700

    Updated chart example.

--- lib/chart_example_page.dart
+++ lib/chart_example_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
@@ -13,19 +15,53 @@ class ChartExamplePage extends StatefulWidget {
 class _ChartExamplePageState extends State<ChartExamplePage> {
   @override
   Widget build(BuildContext context) {
+    Widget _buildRandomChart() {
+      final random = Random();
+      return Padding(
+        padding: const EdgeInsets.all(4),
+        child: SizedBox(
+          width: 50,
+          height: 33,
+          child: _buildLineChart(
+            data: (() {
+              final builder = <_Point>[];
+              for (int i = 0; i < 10; i++) {
+                builder.add(_Point(
+                    domain: i.toDouble(), measure: random.nextInt(1000) / 10));
+              }
+              return builder;
+            })(),
+            base: random.nextInt(1000) / 20,
+            domainFn: _Point.getDomain,
+            measureFn: _Point.getMeasure,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chart Example'),
       ),
       body: SafeArea(
-        child: charts.LineChart(
-          [_prices, _bases],
-          animate: true,
-          domainAxis:
-              const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
-          primaryMeasureAxis:
-              const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
-          // secondaryMeasureAxis: charts.NumericAxisSpec(showAxisLine: false),
+        child: Center(
+          child: Column(
+            children: (() {
+              final builder = <Widget>[
+                TextButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: const Text('Reload'),
+                )
+              ];
+
+              for (int i = 0; i < 15; i++) {
+                builder.add(_buildRandomChart());
+              }
+              return builder;
+            })(),
+          ),
         ),
       ),
     );
@@ -48,38 +84,79 @@ class _ChartExamplePageState extends State<ChartExamplePage> {
   }
 
   // Privates
-  final charts.Series<Price, int> _prices = charts.Series(
-    id: 'STR',
-    data: [
-      Price(time: 0, price: 100),
-      Price(
-        time: 1,
-        price: 200,
-      ),
-      Price(
-        time: 20,
-        price: 400,
-      ),
-    ],
-    domainFn: (datum, _) => datum.time,
-    measureFn: (datum, index) => datum.price,
-    colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-  );
-  final charts.Series<Price, int> _bases = charts.Series(
-    id: 'base',
-    data: [
-      Price(time: 0, price: 250),
-      Price(
-        time: 20,
-        price: 250,
-      ),
-    ],
-    domainFn: (datum, _) => datum.time,
-    measureFn: (datum, index) => datum.price,
-    colorFn: (_, __) => charts.MaterialPalette.yellow.shadeDefault,
-    dashPatternFn: (datum, index) => [20, 2],
-  );
   late final IOWebSocketChannel _channel;
+
+  Widget _buildLineChart<D>({
+    required List<D> data,
+    required double base,
+    required charts.TypedAccessorFn<D, double> domainFn,
+    required charts.TypedAccessorFn<D, double> measureFn,
+  }) {
+    final measures = data.map((e) => measureFn(e, null));
+    final minM = measures.fold<double>(0, min);
+    final maxM = measures.fold<double>(0, max);
+
+    final deltaM = min(base, minM);
+
+    final domains = data.map((e) => domainFn(e, null));
+    final minD = domains.fold<double>(0, min);
+    final maxD = domains.fold<double>(0, max);
+
+    final lastM = measureFn(data.last, data.length - 1);
+    final color = lastM > base
+        ? const charts.Color(r: 0x2E, g: 0xBD, b: 0x85)
+        : lastM == base
+            ? const charts.Color(r: 0xF1, g: 0xCE, b: 0x5F)
+            : const charts.Color(r: 0xE7, g: 0x59, b: 0x6A);
+
+    return charts.LineChart(
+      [
+        charts.Series<_Point, double>(
+          id: 'Base',
+          data: [
+            _Point(domain: minD, measure: base),
+            _Point(domain: maxD, measure: base),
+          ],
+          domainFn: _Point.getDomain,
+          measureFn: (datum, index) => _Point.getMeasure(datum, index) - deltaM,
+          colorFn: (_, __) => const charts.Color(r: 0xC7, g: 0xC7, b: 0xCC),
+          dashPatternFn: (_, __) => [4, 4],
+          strokeWidthPxFn: (datum, index) => 1,
+        ),
+        charts.Series<D, double>(
+          id: 'Price',
+          data: data,
+          domainFn: domainFn,
+          measureFn: (datum, index) => measureFn(datum, index) - deltaM,
+          measureLowerBoundFn: (_, __) => 0,
+          measureUpperBoundFn: (_, __) => maxM - deltaM,
+          colorFn: (_, __) => color,
+          strokeWidthPxFn: (datum, index) => 1,
+        ),
+      ],
+      animate: true,
+      domainAxis: const charts.NumericAxisSpec(
+          showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
+      primaryMeasureAxis: const charts.NumericAxisSpec(
+          showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
+      layoutConfig: charts.LayoutConfig(
+        leftMarginSpec: charts.MarginSpec.fixedPixel(0),
+        rightMarginSpec: charts.MarginSpec.fixedPixel(0),
+        topMarginSpec: charts.MarginSpec.fixedPixel(0),
+        bottomMarginSpec: charts.MarginSpec.fixedPixel(0),
+      ),
+    );
+  }
+}
+
+class _Point {
+  final double domain;
+  final double measure;
+
+  _Point({required this.domain, required this.measure});
+
+  static double getDomain(_Point p, int? _) => p.domain;
+  static double getMeasure(_Point p, int? _) => p.measure;
 }
 
 class Price {
